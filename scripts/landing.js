9aefd5627f4926d1d8126386d4ba88b4fe8937ab
Jared <jprice1990@gmail.com>
2016-08-24 10:41:38 -0500

    Assignment 23 Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,16 +2,14 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
   
-  var revealPoint = function() {
-    for (var i = 0; i < points.length; i++) {
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform = "scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+  var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
   }
   
-  revealPoint();
+  forEach(pointsArray, revealPoint);
 };
 
 window.onload = function() {
